95e1f28d301336cfa9628b24a65247dabd6c9334
lior-amberflo-io <71676688+lior-amberflo-io@users.noreply.github.com>
2020-12-16 16:30:19 -0800

    copy initial

---
+++ setup.py
@@ -0,0 +1,73 @@
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# Don't import analytics-python module here, since deps may not be installed
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'analytics'))
+from version import VERSION
+
+long_description = '''
+Segment is the simplest way to integrate analytics into your application.
+One API allows you to turn on any other analytics service. No more learning
+new APIs, repeated code, and wasted development time.
+
+This is the official python client that wraps the Segment REST API (https://segment.com).
+
+Documentation and more details at https://github.com/segmentio/analytics-python
+'''
+
+install_requires = [
+    "requests>=2.7,<3.0",
+    "six>=1.5",
+    "monotonic>=1.5",
+    "backoff==1.10.0",
+    "python-dateutil>2.1"
+]
+
+tests_require = [
+    "mock==2.0.0",
+    "pylint==1.9.3",
+    "flake8==3.7.9",
+    "coverage==4.5.4"
+]
+
+setup(
+    name='analytics-python',
+    version=VERSION,
+    url='https://github.com/segmentio/analytics-python',
+    author='Segment',
+    author_email='friends@segment.com',
+    maintainer='Segment',
+    maintainer_email='friends@segment.com',
+    test_suite='analytics.test.all',
+    packages=['analytics', 'analytics.test'],
+    license='MIT License',
+    install_requires=install_requires,
+    extras_require={
+        'test': tests_require
+    },
+    description='The hassle-free way to integrate analytics into any python application.',
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
