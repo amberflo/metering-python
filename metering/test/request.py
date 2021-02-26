7c02286d5bd978a8fc544ba2a4e3aeac010bfe56
ofer <ofer@amberflo.com>
2021-02-26 14:14:44 -0800

    builder fix

--- metering/test/request.py
+++ metering/test/request.py
@@ -16,18 +16,6 @@ class TestRequests(unittest.TestCase):
         }]).post()
         self.assertEqual(res.status_code, 200)
 
-    def test_datetime_serialization(self):
-        data = {'created': datetime(2012, 3, 4, 5, 6, 7, 891011)}
-        result = json.dumps(data, cls=json.JSONEncoder)
-        self.assertEqual(result, '{"created": "2012-03-04T05:06:07.891011"}')
-
-    def test_date_serialization(self):
-        today = date.today()
-        data = {'created': today}
-        result = json.dumps(data, cls=json.JSONEncoder)
-        expected = '{"created": "%s"}' % today.isoformat()
-        self.assertEqual(result, expected)
-
     def test_should_not_timeout(self):
         res = RequestManager('e9c6a4fc-e275-4eda-b2f8-353ef196ddb7', batch=[{
             'tenant_id': 'myself',
