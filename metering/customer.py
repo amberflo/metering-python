daeaf70bc5f13434d8b778bff3efd21db554f199
filipelbc <filipelbc@gmail.com>
2022-04-22 15:30:33 -0300

    [feature] Expose get customer by id method

--- metering/customer.py
+++ metering/customer.py
@@ -17,45 +17,55 @@ class CustomerApiClient:
         }
         self.logger = Logger()
 
-    def add_or_update_customer(self, payload, create_customer_in_stripe=False):
-        log = self.logger
-        log.debug("calling customers api", payload)
-
-        endpoint = "{url}/?customerId={customerId}".format(
-            url=url, customerId=payload["customerId"]
+    def get(self, customer_id):
+        """
+        Get customer by id.
+        """
+        endpoint = "{url}/?customerId={customer_id}".format(
+            url=url, customer_id=customer_id
         )
+        self.logger.debug("Getting customer")
         response = requests.get(endpoint, headers=self.headers)
 
-        if response.status_code == 200:
-            updated_response = None
-            customer = response.json()
-            if "customerId" in customer:
-                log.debug("Updating customer")
-                updated_response = requests.put(
-                    url, data=json.dumps(payload), headers=self.headers
-                )
-            else:
-                log.debug("Creating customer")
-                params = (
-                    {"autoCreateCustomerInStripe": "true"}
-                    if create_customer_in_stripe
-                    else None
-                )
-                updated_response = requests.post(
-                    url, data=json.dumps(payload), params=params, headers=self.headers
-                )
+        if response.status_code != 200:
+            self.logger.error(
+                "received response in looking up customer: %s", response.text
+            )
+            raise APIError(response.status_code, response.text, response.text)
 
-            if updated_response.status_code == 200:
-                log.debug("API call successful")
-                return updated_response.json()
+        return response.json()
 
-            log.error("received error: %s", updated_response.text)
-            raise APIError(
-                updated_response.status_code,
-                updated_response.text,
-                updated_response.text,
+    def add_or_update_customer(self, payload, create_customer_in_stripe=False):
+        """
+        Creates a new or updates an existing customer.
+
+        The update has PUT semantics (i.e. it discards existing data).
+
+        `create_customer_in_stripe` is only used when *creating* a new customer.
+        """
+        customer = self.get(payload["customerId"])
+
+        if "customerId" in customer:
+            self.logger.debug("Updating customer")
+            response = requests.put(url, data=json.dumps(payload), headers=self.headers)
+        else:
+            self.logger.debug("Creating customer")
+            params = (
+                {"autoCreateCustomerInStripe": "true"}
+                if create_customer_in_stripe
+                else None
+            )
+            response = requests.post(
+                url, data=json.dumps(payload), params=params, headers=self.headers
             )
 
-        else:
-            log.error("received response in looking up customer: %s", response.text)
-            raise APIError(response.status_code, response.text, response.text)
+        if response.status_code == 200:
+            self.logger.debug("API call successful")
+            return response.json()
+
+        self.logger.error("received error: %s", response.text)
+        raise APIError(
+            response.status_code,
+            response.text,
+            response.text,
+        )
