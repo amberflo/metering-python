8e809fbc659d03fc76cd18fe1fc587619963dad3
filipelbc <filipelbc@gmail.com>
2022-04-29 15:46:10 -0300

    [feature] add customer prepaid order api

---
+++ tests/integration/test_customer_prepaid_order_api_client.py
@@ -0,0 +1,52 @@
+import os
+from time import time
+from uuid import uuid4
+import unittest
+
+from metering.session import ApiSession
+from metering.customer_prepaid_order import (
+    BillingPeriod,
+    BillingPeriodUnit,
+    CustomerPrepaidOrderApiClient,
+    create_customer_prepaid_order_payload,
+)
+
+API_KEY = os.environ.get("TEST_API_KEY")
+
+
+@unittest.skipIf(API_KEY is None, "Needs Amberflo's API key")
+class TestCustomerPrepaidOrderApiClient(unittest.TestCase):
+    def setUp(self):
+        self.customer_id = ApiSession(API_KEY).get("/customers/")[0]["customerId"]
+        self.client = CustomerPrepaidOrderApiClient(API_KEY)
+
+    def test_add_prepaid_order(self):
+        message = create_customer_prepaid_order_payload(
+            id=str(uuid4()),
+            customer_id=self.customer_id,
+            start_time_in_seconds=int(time()),
+            prepaid_price=100,
+            external_payment=True,
+        )
+        response = self.client.add(message)
+        self.assertIsInstance(response, dict)
+        self.assertIsNone(response["recurrenceFrequency"])
+
+    def test_add_prepaid_order_with_recurrence(self):
+        message = create_customer_prepaid_order_payload(
+            id=str(uuid4()),
+            customer_id=self.customer_id,
+            start_time_in_seconds=int(time()),
+            prepaid_price=100,
+            external_payment=True,
+            recurrence_frequency=BillingPeriod(10, BillingPeriodUnit.DAY),
+        )
+        response = self.client.add(message)
+        self.assertIsInstance(response, dict)
+        self.assertEqual(
+            response["recurrenceFrequency"], {"interval": "day", "intervalsCount": 10}
+        )
+
+    def test_list_active_prepaid_orders(self):
+        response = self.client.list_active(self.customer_id)
+        self.assertIsInstance(response, list)
